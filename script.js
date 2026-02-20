6159f3d9024030b98527c6e43f4b7cb03ad0f196
AnaLuiza <nininha_alesilva@hotmail.com>
2026-02-20 17:50:53 -0300

    estilizando a barra de pesquisa

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-// botão de adicionar ao carrinho
-const botoes = document.querySelectorAll("button");
+const botoes = document.querySelectorAll (".btn-adicionar");
+const listaCarrinho = document.getElementById("lista-carrinho");
+const totalElemento = document.getElementById("total");
+
+let total = 0;
 
 botoes.forEach(botao => {
   botao.addEventListener("click", () => {
-    alert("Produto adicionado ao carrinho 🐾");
+
+    const nome = botao.getAttribute("data-nome");
+    const preco = parseFloat(botao.getAttribute("data-preco"));
+
+    // Criar item da lista
+    const item = document.createElement("li");
+    item.innerHTML = `
+      ${nome} - R$ ${preco.toFixed(2)}
+    `;
+
+    listaCarrinho.appendChild(item);
+
+    // Atualizar total
+    total += preco;
+    totalElemento.textContent = `Total: R$ ${total.toFixed(2)}`;
+
   });
 });
